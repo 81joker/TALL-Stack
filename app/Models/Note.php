d8d7e1f46d15067714b3226d6658c1efcce3078b
Nehad <35563834+81joker@users.noreply.github.com>
2024-03-14 11:41:24 +0100

    install laravel

---
+++ app/Models/Note.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Note extends Model
+{
+    use HasFactory, HasUuids;
+    protected $guarded = ['id'];
+
+    public function user(){
+        return  $this->belongsTo(User::class);
+    }
+    // public function publishedNotes(User $user){
+    //     return $this->where('user_id' ,$user->id)->where('is_published')->get();
+    // }
+}
