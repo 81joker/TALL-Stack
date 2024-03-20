7703692c08831b2767a3bad419084be2253c5174
Nehad <35563834+81joker@users.noreply.github.com>
2024-03-20 17:44:03 +0100

    lazy spinner

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -8,7 +8,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="p-6 text-gray-900 dark:text-gray-100">
                 <div>
-                    <livewire:notes.show-notes />
+                    <livewire:notes.show-notes  />
+                    {{-- <livewire:notes.show-notes lazy /> --}}
                 </div>
             </div>
         </div>
