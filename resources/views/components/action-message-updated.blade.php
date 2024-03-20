0fd7ccbbe350d48687090ddb9c301a7ef2261ffb
Nehad <35563834+81joker@users.noreply.github.com>
2024-03-20 16:13:11 +0100

    edit page has been complited

---
+++ resources/views/components/action-message-updated.blade.php
@@ -0,0 +1,10 @@
+@props(['updated'])
+
+<div x-data="{ shown: false, timeout: null }"
+     x-init="@this.on('{{ $updated }}', () => { clearTimeout(timeout); shown = true; timeout = setTimeout(() => { shown = false }, 2000); })"
+     x-show.transition.out.opacity.duration.1500ms="shown"
+     x-transition:leave.opacity.duration.1500ms
+     style="display: none;"
+    {{ $attributes->merge(['class' => 'text-sm text-gray-600 dark:text-gray-400']) }}>
+    {{ $slot->isEmpty() ? 'Updated.' : $slot }}
+</div>
--- resources/views/livewire/notes/edit-note.blade.php
+++ resources/views/livewire/notes/edit-note.blade.php
@@ -4,16 +4,41 @@ use Livewire\Volt\Component;
 use Livewire\Attributes\Layout;
 use App\Models\Note;
 
-new #[Layout('layouts.app')] class  extends Component{ 
-// / new class extends Component {
- public Note $note;
-
- public function mount(Note $note){
-    $this->authorize('update', $note);
-    $this->fill($note);
- }
-
-
+new #[Layout('layouts.app')] class extends Component {
+    // / new class extends Component {
+    public Note $note;
+    public $noteTitle;
+    public $noteBody;
+    public $noteRecipient;
+    public $noteSendDate;
+    public $noteIsPublished;
+    public function mount(Note $note)
+    {
+        $this->authorize('update', $note);
+        $this->fill($note);
+        $this->noteTitle = $note->title;
+        $this->noteBody = $note->body;
+        $this->noteRecipient = $note->recipient;
+        $this->noteSendDate = $note->send_date;
+        $this->noteIsPublished = $note->is_published;
+    }
+    public function saveNote()
+    {
+        $validated = $this->validate([
+            'noteTitle' => ['required', 'string', 'min:5'],
+            'noteBody' => ['required', 'string', 'min:20'],
+            'noteRecipient' => ['required', 'email'],
+            'noteSendDate' => ['required', 'date'],
+        ]);
+        $this->note->update([
+            'title' => $this->noteTitle,
+            'body' => $this->noteBody,
+            'recipient' => $this->noteRecipient,
+            'send_date' => $this->noteSendDate,
+            'is_published' => $this->noteIsPublished,
+        ]);
+        $this->dispatch('note-updated');
+    }
 }; ?>
 
 <div>
@@ -31,7 +56,7 @@ new #[Layout('layouts.app')] class  extends Component{
                     <x-button type="submit" secondary spinner="saveNote">Save Note</x-button>
                     <x-button href="{{ route('notes.index') }}" flat negative>Back to Notes</x-button>
                 </div>
-                <x-action-message on="note-saved" />
+                <x-action-message-updated updated="note-updated" />
                 <x-errors />
             </form>
         </div>
